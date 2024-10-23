71e470ce781459dc0052bf1536b59a0e87a60dff
FelipeSt07 <felipesantacruz-2011@hotmail.com>
2024-10-23 11:56:21 -0500

    Completo modelos y templates

--- album/views.py
+++ album/views.py
@@ -1,5 +1,6 @@
+from django.urls import reverse_lazy
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from album.models import Team
 from album.models import Player
 
@@ -20,3 +21,27 @@ class TeamDetailView(DetailView):
 
 class PlayerDetailView(DetailView):
     model = Player
+
+class TeamCreate(CreateView):
+    model = Team
+    fields = '__all__'
+
+class PlayerCreate(CreateView):
+    model = Player
+    fields = '__all__'
+
+class TeamUpdate(UpdateView):
+    model = Team
+    fields = '__all__'
+
+class PlayerUpdate(UpdateView):
+    model = Player
+    fields = '__all__'
+
+class TeamDelete(DeleteView):
+    model = Team
+    success_url = reverse_lazy('team-list')
+
+class PlayerDelete(DeleteView):
+    model = Player
+    success_url = reverse_lazy('player-list')
